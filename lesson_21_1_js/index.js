4bec648cb0b008ef6d6d7d4677441fcda9976eea
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-06-01 23:33:46 +0300

    commit

--- lesson_21_1_js/index.js
+++ lesson_21_1_js/index.js
@@ -1,26 +1,4 @@
-export const getTitle = () => {
-  const element = document.querySelector(".title").textContent;
-  return element;
+export const setTitle = (text) => {
+  const elem = document.querySelector(".title");
+  elem.textContent = text;
 };
-
-
-
-export const getDescription = () => {
-  const element = document.querySelector(".about").innerText;
-  return element;
-};
-
-
-
-export const getPlans = () => {
-  const element = document.querySelector(".plans").innerHTML;
-  return element;
-};
-
-
-
-export const getGoal = () => {
-  const element = document.querySelector(".goal").outerHTML;
-  return element;
-};
-
