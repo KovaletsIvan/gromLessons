2ecee352c1fb4ec80ef74589eea9219b31447f54
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-06-01 22:48:35 +0300

    commit

--- lesson_21_1_js/index.js
+++ lesson_21_1_js/index.js
@@ -1,11 +1,13 @@
 export const getTitleElement = () => {
   const titleElement = document.querySelector(".title");
   console.dir(titleElement);
+  return titleElement;
 };
 
 export const getInputElement = () => {
   const inputElement = document.querySelector("input");
   console.dir(inputElement);
+  return inputElement;
 };
 
 getTitleElement();
