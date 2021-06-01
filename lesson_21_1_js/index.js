afcf40393c7d26f34f815c700e13018749decdaa
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-06-01 23:26:59 +0300

    commit

--- lesson_21_1_js/index.js
+++ lesson_21_1_js/index.js
@@ -3,25 +3,24 @@ export const getTitle = () => {
   return element;
 };
 
-getTitle();
+
 
 export const getDescription = () => {
   const element = document.querySelector(".about").innerText;
   return element;
 };
 
-getDescription();
+
 
 export const getPlans = () => {
   const element = document.querySelector(".plans").innerHTML;
   return element;
 };
 
-getPlans();
+
 
 export const getGoal = () => {
   const element = document.querySelector(".goal").outerHTML;
   return element;
 };
 
-console.log(getGoal());
