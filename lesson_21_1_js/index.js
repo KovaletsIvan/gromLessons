f5a67d96211320cebd4f15e67f725f0bdda1241a
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-06-02 23:59:02 +0300

    commit

--- lesson_21_1_js/index.js
+++ lesson_21_1_js/index.js
@@ -1,5 +1,7 @@
 export const getSection = (num) => {
   const dataNum = document.querySelector(`span[data-number="${num}"]`);
-  const divParent = dataNum.closest("div");
+  const divParent = dataNum.closest(".box");
   return divParent.dataset.section;
 };
+
+
