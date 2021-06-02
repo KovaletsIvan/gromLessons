c524eab82c256fec6b720d5dd1a62a4cf4744766
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-06-02 21:36:57 +0300

    commit

--- lesson_21_1_js/index.js
+++ lesson_21_1_js/index.js
@@ -1,7 +1,18 @@
-export const createButton = (butoonText) => {
-  const elementButton = document.createElement("button");
-  elementButton.textContent = butoonText;
-  const paretElement = document.querySelector("body");
-
-  paretElement.append(elementButton);
+export const finishList = () => {
+  const elemLi1 = document.createElement("li");
+  const elemLi4 = document.createElement("li");
+  const elemLi6 = document.createElement("li");
+  const elemLi8 = document.createElement("li");
+  const elemUl = document.querySelector(".list");
+  const elemLi5 = document.querySelector(".special");
+  elemLi1.textContent = "1";
+  elemUl.prepend(elemLi1);
+  elemLi4.textContent = "4";
+  elemLi5.before(elemLi4);
+  elemLi6.textContent = "6";
+  elemLi5.after(elemLi6);
+  elemLi8.textContent = "8";
+  elemUl.append(elemLi8);
 };
+
+
