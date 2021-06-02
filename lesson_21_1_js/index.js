a01858a231dcc0e93f8dac5a30f20aa4579719cd
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-06-02 22:40:53 +0300

    commit

--- lesson_21_1_js/index.js
+++ lesson_21_1_js/index.js
@@ -1,10 +1,12 @@
-export const finishForm = () => {
-  const formElement = document.querySelector(".login-form");
-  const newInput = document.createElement("input");
-  newInput.setAttribute("type", "text");
-  newInput.setAttribute("name", "login");
-  formElement.append(newInput);
-  const lastInput = document.querySelector("input");
-  lastInput.setAttribute("type", "password");
-  formElement.prepend(newInput);
+const manageClasses = () => {
+  const oneElem = document.querySelector(".one");
+  oneElem.classList.add("selected");
+  const twoElem = document.querySelector(".two");
+  twoElem.classList.remove("selected");
+  const threeElem = document.querySelector(".three");
+  threeElem.classList.toggle("three_done");
+  const fourElem = document.querySelector(".four");
+  if (Array.from(fourElem.classList).includes("some-class")) {
+    fourElem.classList.add("another-class");
+  }
 };
