c5a61bdd9f9c6f8f737d8f3821e73201dbcddf68
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-06-02 22:12:10 +0300

    commit

--- lesson_21_1_js/index.js
+++ lesson_21_1_js/index.js
@@ -1,18 +1,10 @@
-export const finishList = () => {
-  const elemLi1 = document.createElement("li");
-  const elemLi4 = document.createElement("li");
-  const elemLi6 = document.createElement("li");
-  const elemLi8 = document.createElement("li");
-  const elemUl = document.querySelector(".list");
-  const elemLi5 = document.querySelector(".special");
-  elemLi1.textContent = "1";
-  elemUl.prepend(elemLi1);
-  elemLi4.textContent = "4";
-  elemLi5.before(elemLi4);
-  elemLi6.textContent = "6";
-  elemLi5.after(elemLi6);
-  elemLi8.textContent = "8";
-  elemUl.append(elemLi8);
+export const finishForm = () => {
+  const formElement = document.querySelector(".login-form");
+  const newInput = document.createElement("input");
+  newInput.setAttribute("type", "text");
+  newInput.setAttribute("name", "login");
+  formElement.append(newInput);
+  const lastInput = document.querySelector("input");
+  lastInput.setAttribute("type", "password");
+  formElement.prepend(newInput);
 };
-
-
