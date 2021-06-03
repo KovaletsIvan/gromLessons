0acd30f3d7450d765979dc0a3aac7ed319858b30
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-06-03 23:41:31 +0300

    commit

--- lesson_21_1_js/index.js
+++ lesson_21_1_js/index.js
@@ -6,4 +6,10 @@ const btnElem = document.querySelector(".single-use-btn");
 
 btnElem.addEventListener("click", clickedText);
 
-btnElem.removeEventListener("click", clickedText);
+btnElem.addEventListener("click", function () {
+  let counter = 0;
+  counter += 1;
+  if (counter == 1) {
+    btnElem.removeEventListener("click", clickedText);
+  }
+});
