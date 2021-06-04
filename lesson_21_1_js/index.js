378a81741cb633c922ff161b833f58aa79476ac8
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-06-04 20:35:24 +0300

    commit

--- lesson_21_1_js/index.js
+++ lesson_21_1_js/index.js
@@ -3,5 +3,3 @@ const bodyElem = document.querySelector("body");
 const handleClick = (event) => {
   console.log(event.target.textContent);
 };
-
-// bodyElem.addEventListener("click", handleClick);
