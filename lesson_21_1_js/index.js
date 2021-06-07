808a96ad371a84d5914c0730ea5e3e0137ded38e
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-06-08 00:04:19 +0300

    commit

--- lesson_21_1_js/index.js
+++ lesson_21_1_js/index.js
@@ -1,11 +1,62 @@
-const buttonElem = document.querySelectorAll(".pagination__page");
-
-const handleClick = () => {
-  for (let i = 0; i < buttonElem.length; i++) {
-    buttonElem[i].addEventListener("click", (event) => {
-      console.log(event.target.dataset.pageNumber);
-    });
+const generateNumbersRange = (from, to) => {
+  const result = [];
+  for (let i = from; i <= to; i++) {
+    result.push(i);
   }
+  return result;
 };
 
-handleClick();
+const getLineSeats = () => {
+  return generateNumbersRange(1, 10)
+    .map(
+      (seatNumber) =>
+        `<div class = 'sector__seat' 
+        data-seat-number = '${seatNumber}'
+      ></div>`
+    )
+    .join("");
+};
+
+const getSectorLines = () => {
+  const seatsString = getLineSeats();
+  return generateNumbersRange(1, 10)
+    .map(
+      (lineNumber) =>
+        `<div class = 'sector__line' 
+        data-line-number = '${lineNumber}'
+      >${seatsString}</div>`
+    )
+    .join("");
+};
+
+const arenaElement = document.querySelector(".arena");
+
+const renderArena = () => {
+  const linesString = getSectorLines();
+  const sectorsString = generateNumbersRange(1, 3)
+    .map(
+      (sectorNumber) =>
+        `<div class = 'sector' 
+        data-sector-number = '${sectorNumber}'
+      >${linesString}</div>`
+    )
+    .join("");
+  arenaElement.innerHTML = sectorsString;
+};
+
+const onSeatSelect = (event) => {
+  const isSeat = event.target.classList.contains("sector__seat");
+  if (!isSeat) {
+    return;
+  }
+  const seatNumber = event.target.dataset.seatNumber;
+  const lineNumber = event.target.closest(".sector__line").dataset.lineNumber;
+  const sectorNumber = event.target.closest(".sector").dataset.sectorNumber;
+
+  const selectedSeatElem = document.querySelector(".board__selected-seat");
+  selectedSeatElem.textContent = `S ${sectorNumber} - L ${lineNumber} - S ${seatNumber}`;
+};
+
+arenaElement.addEventListener("click", onSeatSelect);
+
+renderArena();
