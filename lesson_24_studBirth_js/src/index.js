866dc952f61214f13d24ac76858f82918039ef6b
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-06-17 16:38:55 +0300

    commit

--- lesson_24_studBirth_js/src/index.js
+++ lesson_24_studBirth_js/src/index.js
@@ -3,29 +3,30 @@ const studentsBirthDays = (students) => {
     (a, b) => new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate()
   );
   const monthFormatter = new Intl.DateTimeFormat("en", { month: "short" });
-  const arrOfBirth = sortedStudents.map((elem) => [
-    monthFormatter.format(new Date(elem.birthDate)),
-    [],
-  ]);
-  const objectOfresult = Object.fromEntries(arrOfBirth);
+  const objectOfresult = Object.fromEntries(
+    sortedStudents.map((elem) => [
+      monthFormatter.format(new Date(elem.birthDate)),
+      [],
+    ])
+  );
   const arrayOfnames = [];
 
-  for (let i = 0; i < arrOfBirth.length; i += 1) {
+  for (let i = 0; i < sortedStudents.length; i += 1) {
     arrayOfnames.push(
       sortedStudents.filter((elem) => new Date(elem.birthDate).getMonth() === i)
     );
   }
-  const sortedArrayOfnames = arrayOfnames.filter((elem) => elem.length > 0);
+  const filtredArrayOfnames = arrayOfnames.filter((elem) => elem.length > 0);
   const newSort = [];
-  for (let i = 0; i < sortedArrayOfnames.length; i += 1) {
-    newSort.push(sortedArrayOfnames[i].map((elem) => elem.name));
+  for (let i = 0; i < filtredArrayOfnames.length; i += 1) {
+    newSort.push(filtredArrayOfnames[i].map((elem) => elem.name));
   }
 
   let i = 0;
   for (const key in objectOfresult) {
     objectOfresult[key] = newSort[i++];
   }
-  console.log(objectOfresult, sortedStudents);
+  console.log(objectOfresult);
 };
 
 const arrOfStudents = [
@@ -33,7 +34,7 @@ const arrOfStudents = [
   { name: "Ben", birthDate: "01/17/2008" },
   { name: "Sam", birthDate: "03/15/2010" },
   { name: "Ivan", birthDate: "02/06/1986" },
-  { name: "Taras", birthDate: "02/05/2010" },
+  { name: "Taras", birthDate: "02/07/2010" },
 ];
 
 studentsBirthDays(arrOfStudents);
