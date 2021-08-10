7906ecbdc3403e2293acf5426d1c3f59ce621c30
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-08-10 23:11:51 +0300

    commit

---
+++ lesson_34_js/index.js
@@ -0,0 +1,58 @@
+const inputElem = document.querySelectorAll('.form-input');
+const btnElem = document.querySelector('.submit-button');
+const formElem = document.querySelector('.login-form');
+const errorElem = document.querySelector('.error-text');
+
+const url = 'https://61118641c38a0900171f124c.mockapi.io/api/v1/users';
+
+const unDisabledBtn = () => {
+  if (
+    [...inputElem]
+      .map((input) => input.reportValidity())
+      .filter((elem) => elem === true).length === 3
+  ) {
+    btnElem.disabled = false;
+  }
+};
+
+const setDataOnServer = (getUserData) => {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(getUserData),
+  });
+};
+
+const getUserData = (e) => {
+  e.preventDefault();
+  const formData = new FormData(formElem);
+  const userData = [...formData].reduce(
+    (obj, [key, value]) => ({ ...obj, [key]: value }),
+    {}
+  );
+
+  setDataOnServer(userData);
+};
+
+const getUserDataFromServer = () => {
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((result) => {
+      if (result.length === 0) {
+        errorElem.textContent = 'Failed to create user';
+        return;
+      }
+      alert(JSON.stringify(result));
+    });
+};
+
+const clearInput = () => {
+  [...formElem].map((elem) => (elem.value = ''));
+};
+
+[...inputElem].map((input) => input.addEventListener('change', unDisabledBtn));
+btnElem.addEventListener('click', getUserData);
+btnElem.addEventListener('click', getUserDataFromServer);
+btnElem.addEventListener('click', clearInput);
