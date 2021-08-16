ec93e066b5d069994b6968642b0e1e34bccb61f2
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-08-16 18:14:45 +0300

    commit

--- lesson_35_1_js/index.js
+++ lesson_35_1_js/index.js
@@ -1,10 +1,10 @@
-const fetchFunc = (userId) => {
+export const fetchFunc = (userId) => {
   return fetch(`https://api.github.com/users/${userId}`)
     .then((response) => response.json())
     .then((result) => result.blog);
 };
 
-const getUsersBlogs = async (arr) => {
+export const getUsersBlogs = async (arr) => {
   await Promise.all(arr.map((elem) => fetchFunc(elem))).then((result) =>
     console.log(result)
   );
