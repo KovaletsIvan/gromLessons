7668bd70928f87c3d9b8e54f88e3fe9b79c8d1d1
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-08-23 21:40:34 +0300

    commit

--- lesson_21_TodoList_js/dist/scripts/index.js
+++ lesson_21_TodoList_js/dist/scripts/index.js
@@ -1,40 +1,57 @@
-import { setItem, getItem } from './storage.js';
-import { getTasksList, setTask, updateTask } from './tasksGetWhey.js';
+"use strict";
+
+require("core-js/modules/es.array.sort.js");
+
+require("core-js/modules/web.dom-collections.iterator.js");
+
+require("core-js/modules/es.string.trim.js");
+
+require("core-js/modules/es.regexp.to-string.js");
+
+var _storage = require("./storage.js");
+
+var _tasksGetWhey = require("./tasksGetWhey.js");
 
 const btnElem = document.querySelector('.create-task-btn');
 const listItem = document.querySelector('.list');
 
-const renderListItems = (listItems) => {
+const renderListItems = listItems => {
   if (!listItems) {
     return;
   }
+
   listItems.sort((a, b) => new Date(b.date) - new Date(a.date));
   const listElem = document.querySelector('.list');
-  const listItemsElems = listItems
-    .sort((a, b) => a.done - b.done)
-    .map(({ text, done, id }) => {
-      const listItemElem = document.createElement('li');
-      listItemElem.classList.add('list__item');
-      if (done) {
-        listItemElem.classList.add('list__item_done');
-      }
-      const checkBoxElem = document.createElement('input');
-      checkBoxElem.setAttribute('type', 'checkbox');
-      checkBoxElem.dataset.id = id;
-      checkBoxElem.checked = done;
-      checkBoxElem.classList.add('list__item-checkbox');
-      listItemElem.append(checkBoxElem, text);
+  const listItemsElems = listItems.sort((a, b) => a.done - b.done).map(_ref => {
+    let {
+      text,
+      done,
+      id
+    } = _ref;
+    const listItemElem = document.createElement('li');
+    listItemElem.classList.add('list__item');
 
-      return listItemElem;
-    });
+    if (done) {
+      listItemElem.classList.add('list__item_done');
+    }
+
+    const checkBoxElem = document.createElement('input');
+    checkBoxElem.setAttribute('type', 'checkbox');
+    checkBoxElem.dataset.id = id;
+    checkBoxElem.checked = done;
+    checkBoxElem.classList.add('list__item-checkbox');
+    listItemElem.append(checkBoxElem, text);
+    return listItemElem;
+  });
   listElem.append(...listItemsElems);
 };
 
-renderListItems(getItem('tasksList'));
+renderListItems((0, _storage.getItem)('tasksList'));
 
 const createTask = () => {
   const inputElem = document.querySelector('.task-input');
   const text = inputElem.value;
+
   if (!text.trim()) {
     inputElem.value = '';
     return;
@@ -45,53 +62,53 @@ const createTask = () => {
     text,
     done: false,
     date: new Date().toISOString(),
-    id: Math.random().toString(),
+    id: Math.random().toString()
   };
-  setTask(newTask)
-    .then(() => getTasksList())
-    .then((newTasksList) => {
-      setItem('tasksList', newTasksList);
-      listItem.innerHTML = '';
-      renderListItems(getItem('tasksList'));
-    });
+  (0, _tasksGetWhey.setTask)(newTask).then(() => (0, _tasksGetWhey.getTasksList)()).then(newTasksList => {
+    (0, _storage.setItem)('tasksList', newTasksList);
+    listItem.innerHTML = '';
+    renderListItems((0, _storage.getItem)('tasksList'));
+  });
 };
 
-const checkboxChange = (event) => {
+const checkboxChange = event => {
   const isCheckbox = event.target.classList.contains('list__item-checkbox');
+
   if (!isCheckbox) {
     return;
   }
+
   const taskId = event.target.dataset.id;
-  const tasksList = getItem('tasksList');
-  const { text, date } = tasksList.find((task) => task.id === taskId);
+  const tasksList = (0, _storage.getItem)('tasksList');
+  const {
+    text,
+    date
+  } = tasksList.find(task => task.id === taskId);
   const done = event.target.checked;
   const updatedTask = {
     text,
     date,
     done,
-    finishDate: done ? new Date().toISOString() : null,
+    finishDate: done ? new Date().toISOString() : null
   };
-  updateTask(taskId, updatedTask)
-    .then(() => getTasksList())
-    .then((newTasksList) => {
-      setItem('tasksList', newTasksList);
-      listItem.innerHTML = '';
-      renderListItems(getItem('tasksList'));
-    });
+  (0, _tasksGetWhey.updateTask)(taskId, updatedTask).then(() => (0, _tasksGetWhey.getTasksList)()).then(newTasksList => {
+    (0, _storage.setItem)('tasksList', newTasksList);
+    listItem.innerHTML = '';
+    renderListItems((0, _storage.getItem)('tasksList'));
+  });
 };
 
 const checkStorage = () => {
   listItem.innerHTML = '';
-  renderListItems(getItem('tasksList'));
+  renderListItems((0, _storage.getItem)('tasksList'));
 };
 
 document.addEventListener('DOMContentLoadet', () => {
-  getTasksList().then((tasksList) => {
-    setItem('tasksList', tasksList);
-    renderListItems(getItem('tasksList'));
+  (0, _tasksGetWhey.getTasksList)().then(tasksList => {
+    (0, _storage.setItem)('tasksList', tasksList);
+    renderListItems((0, _storage.getItem)('tasksList'));
   });
 });
-
 window.addEventListener('storage', checkStorage);
 listItem.addEventListener('click', checkboxChange);
 btnElem.addEventListener('click', createTask);
--- lesson_21_TodoList_js/dist/scripts/storage.js
+++ lesson_21_TodoList_js/dist/scripts/storage.js
@@ -1,7 +1,16 @@
-export const setItem = (key, value) => {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getItem = exports.setItem = void 0;
+
+const setItem = (key, value) => {
   localStorage.setItem(key, JSON.stringify(value));
 };
 
-export const getItem = (key) => JSON.parse(localStorage.getItem(key));
+exports.setItem = setItem;
 
+const getItem = key => JSON.parse(localStorage.getItem(key));
 
+exports.getItem = getItem;
