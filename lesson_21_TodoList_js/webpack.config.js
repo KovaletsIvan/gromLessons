765d8a11e9ce39e55ae34a65837f26e55b6b00e3
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-08-29 21:29:40 +0300

    commit

--- lesson_21_TodoList_js/webpack.config.js
+++ lesson_21_TodoList_js/webpack.config.js
@@ -1,45 +1,45 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const  webpack  = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bandle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /.js$/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.s?css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              outputPath: 'images',
-              limit: 81982,
-            },
-          },
-        ],
-      },
+module.exports = (env, argv) => {
+  const isProdaction = argv.mode === 'production';
+  const config = {
+    entry: './src/index.js',
+    output: {
+      filename: 'bandle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s?css$/i,
+          use: [
+            isProdaction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
     ],
-  },
-  plugins: [
-    new webpack.ProgressPlugin(),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-  ],
+    devServer: {
+      port: 9000,
+      hot: true,
+    },
+  };
+
+  if (isProdaction) {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      })
+    );
+  }
+
+  return config;
 };
