7257db4588819fdbaf4827bbd4305f5dc59530e9
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-08-29 21:16:19 +0300

    commit

--- f-e_tools-8-1/webpack.config.js
+++ f-e_tools-8-1/webpack.config.js
@@ -2,30 +2,44 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bandle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s?css$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
+module.exports = (env, argv) => {
+  const isProdaction = argv.mode === 'production';
+  const config = {
+    entry: './src/index.js',
+    output: {
+      filename: 'bandle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s?css$/i,
+          use: [
+            isProdaction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-  devServer: {
-    port: 9000,
-    hot: true,
-  },
+    devServer: {
+      port: 9000,
+      hot: true,
+    },
+  };
+
+  if (isProdaction) {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      })
+    );
+  }
+
+  return config;
 };
